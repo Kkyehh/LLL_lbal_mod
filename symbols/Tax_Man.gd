1085489af7c62124b28e2db3925020f63280d5c1
Kkyehh <almightyantibot@gmail.com>
2021-09-20 21:28:53 -0700

    change to using dynamic values

--- symbols/Tax_Man.gd
+++ symbols/Tax_Man.gd
@@ -15,6 +15,13 @@ func init(modloader: Reference, params):
     self.description = "Gives <icon_coin><color_FBF236><value_1><end> more for <color_E14A68>each<end> adjacent symbol that gives <icon_coin><color_FBF236>0<end>. Gives <color_E14A68><value_2>x<end> the <icon_coin> <color_E14A68>lost<end> to adjacent symbols that give <color_E14A68>less than<end> <icon_coin><color_FBF236>0<end>."
 
 func add_conditional_effects(symbol, adjacent):
+
+    
     for i in adjacent:
+        var i_effect = effect().if_final_value_less_than(0).set_target(symbol).change_value_bonus(0).dynamic_symbol_value(i, values[1] * -1, true)
+
+        i_effect.effect_dictionary.erase("diff")
+
         symbol.add_effect_for_symbol(i, effect().if_final_value_equals(0).if_type("empty").negate().set_target(symbol).change_value_bonus(values[0]))
-        symbol.add_effect_for_symbol(i, effect().if_final_value_less_than(0).set_target(symbol).change_value_bonus(i.non_prev_final_value * (-1 * values[1])).animate("shake", 0, [symbol, i]))
+
+        symbol.add_effect_for_symbol(i, i_effect)
