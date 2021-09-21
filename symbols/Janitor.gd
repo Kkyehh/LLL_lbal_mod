4e1eaff7c70df6545c4b4758e08a89c566bcab13
Kkyehh <almightyantibot@gmail.com>
2021-09-20 21:29:20 -0700

    buff janitor, use dynamic values instead of static

--- symbols/Janitor.gd
+++ symbols/Janitor.gd
@@ -4,16 +4,22 @@ func init(modloader: Reference, params):
     
     self.id = "janitor"
     self.value = 1
-    self.values = [7]
+    self.values = [8]
     self.rarity = "common"
     self.groups = ["human", "organism", "doglikes"]
     self.sfx = ["farmer"]
     
     self.texture = load_texture("res://LLL_lbal_mod/symbols/Janitor.png")
     self.name = "Janitor"
-    self.description = "<color_E14A68>Destroys<end> adjacent <icon_banana_peel>. Gives <icon_coin><color_FBF236><value_1><end> for each <icon_banana_peel> <color_E14A68>destroyed<end>."
+    self.description = "<color_E14A68>Destroys<end> adjacent <icon_banana_peel>. Gives <color_E14A68><value_1>x<end> the value of <color_E14A68>destroyed<end> symbols."
 	
 func add_conditional_effects(symbol, adjacent):
     for i in adjacent:
+
+        var i_effect = effect().if_destroyed().if_type("banana_peel").set_target(symbol).change_value_bonus(0).dynamic_symbol_value(i, values[0], true).animate("circle", 0, [symbol, i])
+
+        i_effect.effect_dictionary.erase("diff")
+
         symbol.add_effect_for_symbol(i, effect().if_type("banana_peel").set_destroyed())
-        symbol.add_effect_for_symbol(i, effect().if_destroyed().if_type("banana_peel").set_target(symbol).change_value_bonus(values[0]).animate("circle", 0, [symbol, i]))
+
+        symbol.add_effect_for_symbol(i, i_effect)
