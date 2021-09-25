5c0c9ee010039226e97834041a3423b2d6fbb5f6
Kkyehh <almightyantibot@gmail.com>
2021-09-25 12:36:46 -0700

    minor fix

--- symbols/Tax_Man.gd
+++ symbols/Tax_Man.gd
@@ -18,7 +18,7 @@ func add_conditional_effects(symbol, adjacent):
 
     
     for i in adjacent:
-        var i_effect = effect().if_final_value_less_than(0).set_target(symbol).change_value_bonus(0).dynamic_symbol_value(i, values[1] * -1, true).animate("shake", 0, [symbol, i])
+        var i_effect = effect().if_final_value_less_than(0).set_target(symbol).change_value_bonus(0).dynamic_symbol_value(i, -values[1], true).animate("shake", 0, [symbol, i])
 
         i_effect.effect_dictionary.erase("diff")
 
